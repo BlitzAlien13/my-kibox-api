a93ef4b8cb4c91c6bd4982b1215280c8245c47eb
BlitzAlien13 <blitza922@gmail.com>
2025-09-17 12:56:17 +0200

    Bug Fix

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ from KIBox import FakeNews
 app = FastAPI()
 origins = [
     "https://faktenchcker.netlify.app",
-    "https://faktenchcker.netlify.app/"
+    "https://faktenchcker.netlify.app/",
+    "http://localhost:3000",         
+    "http://127.0.0.1:3000",
 ]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins, 
     allow_credentials=True,
-    allow_methods=["OPTIONS", "POST", "GET", "HEAD"],
-    allow_headers=["Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 kibox = KIBox(kibox_instance=None)
