568f0abde5e84c55af1f76de1359842d18c764ad
BlitzAlien13 <blitza922@gmail.com>
2025-09-24 15:47:33 +0200

    Bug Fix

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "https://faktenchecker.netlify.app",
     "https://faktenchecker.netlify.app/",
+    "https://faktenchecker.netlify.app/chat.html",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
